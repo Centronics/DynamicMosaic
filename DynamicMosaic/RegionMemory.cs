73a4d2d67b0e938937926645d5dc609f173589fa
Илья <kras-05@mail.ru>
2017-03-21 00:25:11 +0300

    Начало разработки ассоциативного класса.

---
+++ DynamicMosaic/RegionMemory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using DynamicParser;
+
+namespace DynamicMosaic
+{
+    /// <summary>
+    /// Предназначен для связывания ассоциаций.
+    /// </summary>
+    public sealed class LinkedRegion
+    {
+        /// <summary>
+        /// Список ассоциаций с текущими ассоциациями.
+        /// </summary>
+        readonly List<LinkedRegion> _linkedRegions = new List<LinkedRegion>();
+
+        /// <summary>
+        /// Текущая связываемая карта.
+        /// </summary>
+        public Processor CurrentProcessor { get; set; }
+    }
+
+    /// <summary>
+    /// Предназначен для запоминания образов и ассоциации их с другими образами.
+    /// </summary>
+    public sealed class RegionMemory
+    {
+        /// <summary>
+        /// Карты, с которыми производятся ассоциации.
+        /// </summary>
+        readonly ProcessorContainer _processorContainer;
+
+        /// <summary>
+        /// Инициализирует текущий экземпляр картами, с которыми в дальнейшем будет производиться ассоциация других карт.
+        /// </summary>
+        /// <param name="processorContainer">Набор исходных карт.</param>
+        public RegionMemory(ProcessorContainer processorContainer)
+        {
+            if (processorContainer == null)
+                throw new ArgumentNullException(nameof(processorContainer), $@"{nameof(RegionMemory)}: Набор исходных карт равен null.");
+            if (processorContainer.Count <= 0)
+                throw new ArgumentException($@"{nameof(RegionMemory)}: Набор исходных карт пустой ({processorContainer.Count}).", nameof(processorContainer));
+            _processorContainer = processorContainer;
+        }
+
+
+    }
+}
