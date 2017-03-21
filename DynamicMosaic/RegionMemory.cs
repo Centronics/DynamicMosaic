8ed8cf03fca630a23fd9859d896c26fbab2474dc
Илья <kras-05@mail.ru>
2017-03-22 00:21:42 +0300

    Продолжение разработки ассоциативного класса RegionMemory.

--- DynamicMosaic/RegionMemory.cs
+++ DynamicMosaic/RegionMemory.cs
@@ -1,48 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DynamicParser;
 
 namespace DynamicMosaic
 {
     /// <summary>
-    /// Предназначен для связывания ассоциаций.
-    /// </summary>
-    public sealed class LinkedRegion
-    {
-        /// <summary>
-        /// Список ассоциаций с текущими ассоциациями.
-        /// </summary>
-        readonly List<LinkedRegion> _linkedRegions = new List<LinkedRegion>();
-
-        /// <summary>
-        /// Текущая связываемая карта.
-        /// </summary>
-        public Processor CurrentProcessor { get; set; }
-    }
-
-    /// <summary>
-    /// Предназначен для запоминания образов и ассоциации их с другими образами.
+    ///     Предназначен для связывания ассоциаций.
     /// </summary>
     public sealed class RegionMemory
     {
         /// <summary>
-        /// Карты, с которыми производятся ассоциации.
+        ///     Текущие связываемые карты.
         /// </summary>
-        readonly ProcessorContainer _processorContainer;
+        readonly ProcessorContainer _currentProcessors;
 
         /// <summary>
-        /// Инициализирует текущий экземпляр картами, с которыми в дальнейшем будет производиться ассоциация других карт.
+        ///     Список классов для сопоставления.
         /// </summary>
-        /// <param name="processorContainer">Набор исходных карт.</param>
-        public RegionMemory(ProcessorContainer processorContainer)
+        readonly List<RegionMemory> _nextLinkedRegion = new List<RegionMemory>();
+
+        /// <summary>
+        ///     Инициализирует текущий экземпляр указанными картами.
+        /// </summary>
+        /// <param name="processors">Карты, которые необходимо ассоциировать.</param>
+        public RegionMemory(params Processor[] processors)
         {
-            if (processorContainer == null)
-                throw new ArgumentNullException(nameof(processorContainer), $@"{nameof(RegionMemory)}: Набор исходных карт равен null.");
-            if (processorContainer.Count <= 0)
-                throw new ArgumentException($@"{nameof(RegionMemory)}: Набор исходных карт пустой ({processorContainer.Count}).", nameof(processorContainer));
-            _processorContainer = processorContainer;
+            if (processors == null)
+                throw new ArgumentNullException(nameof(processors),
+                    $"{nameof(RegionMemory)}: Сопоставляемая карта должна быть указана.");
+            if (processors.Length <= 0)
+                throw new ArgumentException(
+                    $"{nameof(RegionMemory)}: Количество сопоставляемых карт должно быть больше ноля.",
+                    nameof(processors));
+            _currentProcessors = new ProcessorContainer(processors);
+            Width = _currentProcessors.Width;
+            Height = _currentProcessors.Height;
         }
 
+        /// <summary>
+        ///     Инициализирует текущий экземпляр указанными картами.
+        /// </summary>
+        /// <param name="processors">Карты, которые необходимо ассоциировать.</param>
+        public RegionMemory(IList<Processor> processors)
+        {
+            if (processors == null)
+                throw new ArgumentNullException(nameof(processors),
+                    $"{nameof(RegionMemory)}: Сопоставляемая карта должна быть указана.");
+            if (processors.Count <= 0)
+                throw new ArgumentException(
+                    $"{nameof(RegionMemory)}: Количество сопоставляемых карт должно быть больше ноля.",
+                    nameof(processors));
+            _currentProcessors = new ProcessorContainer(processors);
+            Width = _currentProcessors.Width;
+            Height = _currentProcessors.Height;
+        }
 
+        /// <summary>
+        ///     Инициализирует текущий экземпляр указанными картами.
+        /// </summary>
+        /// <param name="pc">Карты, которые необходимо ассоциировать.</param>
+        public RegionMemory(ProcessorContainer pc)
+        {
+            if (pc == null)
+                throw new ArgumentNullException(nameof(pc),
+                    $"{nameof(RegionMemory)}: Сопоставляемые карты должны быть указаны.");
+            if (pc.Count <= 0)
+                throw new ArgumentException(
+                    $"{nameof(RegionMemory)}: Количество сопоставляемых карт должно быть больше ноля.", nameof(pc));
+            _currentProcessors = new ProcessorContainer(pc[0]);
+            for (int k = 1; k < pc.Count; k++)
+                _currentProcessors.Add(pc[k]);
+            Width = pc.Width;
+            Height = pc.Height;
+        }
+
+        /// <summary>
+        ///     Размер связываемых карт по ширине.
+        /// </summary>
+        public int Width { get; }
+
+        /// <summary>
+        ///     Размер связываемых карт по высоте.
+        /// </summary>
+        public int Height { get; }
+
+        /// <summary>
+        ///     Добавляет новые карты, которые необходимо ассоциировать с текущим объектом.
+        ///     В случае необходимости создать слой для добавления карты, слой создаётся автоматически.
+        /// </summary>
+        /// <param name="pc">Карты, которые необходимо ассоциировать.</param>
+        public void AddAssociation(ProcessorContainer pc)
+        {
+            if (pc == null)
+                throw new ArgumentNullException(nameof(pc),
+                    $"{nameof(AddAssociation)}: Ассоциируемые карты должны быть указаны.");
+            if (pc.Count <= 0)
+                return;
+            if (pc.Width == Width && pc.Height == Height)
+            {
+                for (int k = 0; k < pc.Count; k++)
+                    _currentProcessors.Add(pc[k]);
+                return;
+            }
+            RegionMemory rm = FindRegion(pc.Width, pc.Height);
+            if (rm == null)
+            {
+                _nextLinkedRegion.Add(new RegionMemory(pc));
+                return;
+            }
+            rm.AddAssociation(pc);
+        }
+
+        /// <summary>
+        ///     Выполняет поиск и возвращает <see cref="RegionMemory" /> или null в зависимости от того, содержится указанный
+        ///     уровень или нет.
+        /// </summary>
+        /// <param name="width">Искомая ширина.</param>
+        /// <param name="height">Искомая высота.</param>
+        /// <returns>Возвращает <see cref="RegionMemory" /> или null в зависимости от того, содержится указанный уровень или нет.</returns>
+        RegionMemory FindRegion(int width, int height)
+        {
+            if (width <= 0)
+                throw new ArgumentException($"{nameof(FindRegion)}: Ширина меньше или равна нолю ({width}).",
+                    nameof(width));
+            if (height <= 0)
+                throw new ArgumentException($"{nameof(FindRegion)}: Высота меньше или равна нолю ({height}).",
+                    nameof(height));
+            if (Width == width && Height == height)
+                return this;
+            return _nextLinkedRegion.FirstOrDefault(rm => rm.Width == width && rm.Height == height);
+        }
+
+        /// <summary>
+        ///     Выполняет поиск и возвращает значение, показывающее, содержится ли такой уровень или нет.
+        /// </summary>
+        /// <param name="width">Искомая ширина.</param>
+        /// <param name="height">Искомая высота.</param>
+        /// <returns>Возвращает значение true в случае нахождения указанного уровня, в противном случае - false.</returns>
+        public bool Contains(int width, int height) => FindRegion(width, height) != null;
     }
 }
