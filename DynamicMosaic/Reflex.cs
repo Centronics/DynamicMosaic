5a4943ef56276c6ee901cc2cce0e927c0823dd3f
Илья <kras-05@mail.ru>
2017-05-08 00:16:49 +0300

    Подготовка к новой версии.

--- DynamicMosaic/Reflex.cs
+++ DynamicMosaic/Reflex.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using DynamicParser;
 
 namespace DynamicMosaic
@@ -19,11 +18,6 @@ namespace DynamicMosaic
         readonly ProcessorContainer _seaProcessors;
 
         /// <summary>
-        /// Рефлексы, которые система испытывала при предыдущих запросах.
-        /// </summary>
-        readonly List<Reflex> _lstReflexs = new List<Reflex>();
-
-        /// <summary>
         /// Получает карту, поиск которой производится при каждом запросе поиска слова.
         /// </summary>
         /// <param name="index">Индекс карты.</param>
@@ -31,21 +25,9 @@ namespace DynamicMosaic
         public Processor GetProcessor(int index) => _seaProcessors[index];
 
         /// <summary>
-        /// Получает <see cref="Reflex"/> по указанному индексу.
-        /// </summary>
-        /// <param name="index">Индекс <see cref="Reflex"/>.</param>
-        /// <returns>Возвращает <see cref="Reflex"/> по указанному индексу.</returns>
-        public Reflex GetReflex(int index) => _lstReflexs[index];
-
-        /// <summary>
         /// Получает количество карт в контексте.
         /// </summary>
         public int CountProcessor => _seaProcessors.Count;
-
-        /// <summary>
-        /// Получает количество рефлексов в контексте.
-        /// </summary>
-        public int CountReflexs => _lstReflexs.Count;
 
         /// <summary>
         /// Инициализирует текущий контекст указанными картами.
@@ -93,8 +75,6 @@ namespace DynamicMosaic
             StringBuilder sb = new StringBuilder();
             if (!processor.GetEqual(_seaProcessors).FindRelation(word))
                 return null;
-            if (IsAllMaps(sb.ToString()))
-                return this;
             List<Processor> lstProcessors = new List<Processor>();
             foreach (char c in word.Select(str => str))
                 for (int k = 0; k < _seaProcessors.Count; k++)
@@ -110,73 +90,6 @@ namespace DynamicMosaic
             if (_lstReflexs.All(re => !re.IsSimilar(reflex)))
                 _lstReflexs.Add(reflex);
             return reflex;
-        }
-
-        /// <summary>
-        /// Находит указанное слово, используя карты, находящиеся в коллекции.
-        /// </summary>
-        /// <param name="word">Искомое слово.</param>
-        /// <param name="processor">Карта, на которой производится поиск искомого слова.</param>
-        /// <returns>Возвращает коллекцию подходящих карт.</returns>
-        ConcurrentBag<Reflex> FindWordLst(string word, Processor processor)
-        {
-            if (_lstReflexs.Count <= 0)
-                return null;
-            if (word == null)
-                throw new ArgumentNullException(nameof(word), $"{nameof(FindWordLst)}: Искомое слово должно быть указано.");
-            if (word == string.Empty)
-                throw new ArgumentException($"{nameof(FindWordLst)}: Искомое слово должно быть указано.", nameof(word));
-            if (processor == null)
-                throw new ArgumentNullException(nameof(processor), $"{nameof(FindWordLst)}: Карта, на которой производится поиск, не указана.");
-            ConcurrentBag<Reflex> lstReflexs = new ConcurrentBag<Reflex>();
-            string errString = string.Empty, errStopped = string.Empty;
-            bool exThrown = false, exStopped = false;
-            Parallel.ForEach(_lstReflexs, (k, state) =>
-            {
-                try
-                {
-                    Reflex reflex = k.FindWord(processor, word);
-                    if (reflex == null)
-                        return;
-                    if (reflex != k)
-                        lstReflexs.Add(reflex);
-                }
-                catch (Exception ex)
-                {
-                    try
-                    {
-                        errString = ex.Message;
-                        exThrown = true;
-                        state.Stop();
-                    }
-                    catch (Exception ex1)
-                    {
-                        errStopped = ex1.Message;
-                        exStopped = true;
-                    }
-                }
-            });
-            if (exThrown)
-                throw new Exception(exStopped ? $@"{errString}{Environment.NewLine}{errStopped}" : errString);
-            return lstReflexs;
-        }
-
-        /// <summary>
-        /// Возвращает результат, говорящий о том, присутствует ли в текущем контексте <see cref="Reflex"/> с такими же поисковыми картами,
-        /// как указано в запросе.
-        /// </summary>
-        /// <param name="reflex">Искомый контекст <see cref="Reflex"/>.</param>
-        /// <returns>Возвращает результат true в случае нахождения подобной карты, false - в противном случае.</returns>
-        bool IsSimilar(Reflex reflex)
-        {
-            if (reflex == null)
-                throw new ArgumentNullException(nameof(reflex), $"{nameof(IsSimilar)}: Искомый контекст должен быть указан.");
-            if (_seaProcessors.Count != reflex._seaProcessors.Count)
-                return false;
-            for (int k = 0; k < _seaProcessors.Count; k++)
-                if (!reflex._seaProcessors.ContainsTag(_seaProcessors[k].Tag))
-                    return false;
-            return true;
         }
 
         /// <summary>
@@ -198,29 +111,14 @@ namespace DynamicMosaic
         }
 
         /// <summary>
-        /// Проверяет, содержат ли карты в свойстве <see cref="Processor.Tag"/> все буквы указанного слова.
+        /// Получает карты, между которыми отсутствуют конфликты, представляющие заданное слово.
+        /// Возвращаются в карты с одинаковыми названиями.
         /// </summary>
         /// <param name="word">Проверяемое слово.</param>
-        /// <returns>Возвращает значение true в случае, если текущий экземпляр полностью описывает указанное слово, в противном случае - false.</returns>
-        bool IsAllMaps(string word)
+        /// <returns>Возвращает карты, между которыми отсутствуют конфликты, представляющие заданное слово.</returns>
+        IEnumerable<Processor> GetProcessors(string word)
         {
-            if (string.IsNullOrEmpty(word))
-                throw new ArgumentNullException(nameof(word), $"{nameof(IsAllMaps)}: Искомое слово должно быть задано.");
-            if (word.Length != _seaProcessors.Count)
-                return false;
-            bool[] maps = new bool[_seaProcessors.Count];
-            foreach (char c in word)
-                for (int k = 0; k < _seaProcessors.Count; k++)
-                    if (char.ToUpper(_seaProcessors[k].Tag[0]) == c)
-                        maps[k] = true;
-            return maps.All(b => b);
+            
         }
-
-        /// <summary>
-        /// Находит контекст, полностью соответствующий заданному по своему потенциалу.
-        /// </summary>
-        /// <param name="reflex">Искомый потенциал.</param>
-        /// <returns>Возвращает контекст, полностью соответствующий заданному по своему потенциалу.</returns>
-        Reflex FindSimilar(Reflex reflex) => _lstReflexs.FirstOrDefault(r => r.IsSimilar(reflex));
     }
 }
