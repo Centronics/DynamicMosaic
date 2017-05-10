ff9b6b86a06a9b56c9f721aaedc271cac5f4b406
Илья <kras-05@mail.ru>
2017-05-11 00:43:29 +0300

    Добавление класса ReflexCollection.

---
+++ DynamicMosaic/ReflexCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DynamicMosaic
+{
+    /// <summary>
+    /// Представляет концепцию всеобъёмлющего анализа входных данных с помощью класса <see cref="Reflex"/>.
+    /// </summary>
+    public sealed class ReflexCollection
+    {
+        /// <summary>
+        /// Внутреннее хранилище ранее загруженных данных.
+        /// </summary>
+        readonly List<Reflex> _reflexs = new List<Reflex>();
+
+        /// <summary>
+        /// Находит связь между заданным словом и текущими картами объекта <see cref="ReflexCollection"/>.
+        /// </summary>
+        /// <param name="word">Искомое слово.</param>
+        /// <returns>В случае нахождения связи возвращает значение <see langword="true"/>, в противном случае - <see langword="false"/>.</returns>
+        public bool FindRelation(string word)
+        {
+
+        }
+    }
+}
