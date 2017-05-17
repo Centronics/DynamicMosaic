75e7dd7ec5586237f0f8416bd05a466d3e3bcf45
Илья <kras-05@mail.ru>
2017-05-18 02:15:07 +0300

    Завершена разработка Reflector.

--- DynamicMosaic/Reflector.cs
+++ DynamicMosaic/Reflector.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DynamicParser;
 
 namespace DynamicMosaic
 {
-    /// <summary>
-    /// Представляет один "разряд", т.е. 
-    /// </summary>
-    struct ReflexCollector
-    {
-
-    }
-
     /// <summary>
     /// Пара "искомое значение - поле для поиска".
     /// </summary>
@@ -68,12 +61,7 @@ namespace DynamicMosaic
         /// <summary>
         /// Анализирует входные данные.
         /// </summary>
-        readonly ReflexCollection _reflexCollection;
-
-        /// <summary>
-        /// Содержит результаты предыдущих запросов.
-        /// </summary>
-        readonly List<ReflexCollection> _startReflexCollections = new List<ReflexCollection>();
+        ReflexCollection _reflexCollection;
 
         /// <summary>
         /// Инициализирует текущий экземпляр объектом <see cref="Reflex"/>.
@@ -103,18 +91,77 @@ namespace DynamicMosaic
         }
 
         /// <summary>
-        /// 
+        /// Находит связь между заданным словом и данными в контексте.
+        /// В случае нахождения связи возвращает значение <see langword="true"/>, в противном случае - <see langword="false"/>.
         /// </summary>
-        /// <param name="word"></param>
-        /// <returns></returns>
-        public bool Test(string word)
+        /// <param name="word">Проверяемое слово.</param>
+        /// <returns>В случае нахождения связи возвращает значение <see langword="true"/>, в противном случае - <see langword="false"/>.</returns>
+        public bool FindRelation(string word)
         {
-
+            if (string.IsNullOrEmpty(word))
+                return false;
+            _reflexCollection = (ReflexCollection)_reflexCollection.Clone();//можно добавить метод Clear
+            List<PairWordValue> lstPairWordValues = new List<PairWordValue>();
+            for (int z = 1; z < _pairs.Count; z++)
+                for (int k = 1; k < _pairs.Count; k++)
+                {
+                    int[] count = new int[z];
+                    while (ChangeCount(count, k) != -1)
+                    {
+                        GetWord(count, lstPairWordValues);
+                        foreach (PairWordValue p in lstPairWordValues)
+                        {
+                            if (p.IsEmpty)
+                                throw new Exception($@"{nameof(FindRelation)}: {nameof(PairWordValue)} пустая.");
+                            if (_reflexCollection.FindRelation(p.Field, p.FindString))
+                                return true;
+                        }
+                        _reflexCollection = (ReflexCollection)_reflexCollection.Clone();
+                    }
+                }
+            return false;
         }
 
-        void GetSequencePairs(ref byte[] bytes)
+        /// <summary>
+        ///     Увеличивает значение старших разрядов счётчика букв, если это возможно.
+        ///     Если увеличение было произведено, возвращается номер позиции, на которой произошло изменение, в противном случае
+        ///     -1.
+        /// </summary>
+        /// <param name="count">Массив-счётчик.</param>
+        /// <param name="maxCount">Максимальное значение, которого может достигать счётчик в одном разряде.</param>
+        /// <returns>Возвращается номер позиции, на которой произошло изменение, в противном случае -1.</returns>
+        int ChangeCount(int[] count, int maxCount)
         {
-            
+            if (count == null || count.Length <= 0)
+                throw new ArgumentException($"{nameof(ChangeCount)}: Массив-счётчик не указан или его длина некорректна ({count?.Length}).", nameof(count));
+            if (maxCount <= 0)
+                throw new ArgumentException($@"{nameof(ChangeCount)}: Значение {nameof(maxCount)} должно быть больше ноля ({maxCount}).", nameof(maxCount));
+            for (int k = count.Length - 1; k >= 0; k--)
+            {
+                if (count[k] >= maxCount - 1) continue;
+                count[k]++;
+                for (int x = k + 1; x < count.Length; x++)
+                    count[x] = 0;
+                return k;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        ///     Генерирует слово из частей, содержащихся в коллекции, основываясь на данных счётчиков.
+        /// </summary>
+        /// <param name="count">Данные счётчиков по каждому слову.</param>
+        /// <param name="lstPairs">Коллекция, в которую будут добавлены элементы, при этом будут удалены существующие.</param>
+        void GetWord(IList<int> count, List<PairWordValue> lstPairs)
+        {
+            if (count == null)
+                throw new ArgumentNullException(nameof(count), $"{nameof(GetWord)}: Массив данных равен null.");
+            if (count.Count <= 0)
+                throw new ArgumentException($"{nameof(GetWord)}: Длина массива данных должна совпадать с количеством хранимых слов.", nameof(count));
+            if (lstPairs == null)
+                throw new ArgumentNullException(nameof(lstPairs), $@"{nameof(GetWord)}: Массив коллекций равен null.");
+            lstPairs.Clear();
+            lstPairs.AddRange(count.Select(c => _pairs[c]));
         }
     }
 }
